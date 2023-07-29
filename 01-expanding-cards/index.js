0511c2051303c9cd10ce4f55029f7151394ed9cb
Dennnis Mburu Mwaura <dennismburu2063@gmail.com>
2023-07-29 12:01:57 +0300

    expanding cards completed

--- 01-expanding-cards/index.js
+++ 01-expanding-cards/index.js
@@ -1,1 +1,14 @@
-console.log("hello world");
+const panels = document.querySelectorAll(".panel");
+
+panels.forEach((panel) => {
+  panel.addEventListener("click", () => {
+    removeActiveClass();
+    panel.classList.add("active");
+  });
+});
+
+const removeActiveClass = () => {
+  panels.forEach((panel) => {
+    panel.classList.remove("active");
+  });
+};
